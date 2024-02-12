0c6f92d9a36bd577dc567375e5bd12aef8cc2b73
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-02-12 16:51:25 +1100

    import stylesheet for html, body

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app";
-//import "./styles/main.css"; // Relative path to styles/main.css
+import "./styles/main.css"; // Relative path to styles/main.css
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
