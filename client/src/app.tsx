6a2ffcbd134254d9578d58450f529f4e569556e6
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-02-12 16:51:07 +1100

    loaded main container component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
+import MainContainer from "./MainContainer";
 
 const App: React.FC = () => {
-  return <div className="root">Test</div>;
+  return (
+    <div style={{ padding: "0", height: "100%", backgroundColor: "gray" }}>
+      <MainContainer />
+    </div>
+  );
 };
 
 export default App;
