eebbb9bd2867a2241ea341fe5de70db749a5c39c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-03 12:03:52 +1100

    dummy component

---
+++ client/src/EditTask.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+const EditTask: React.FC = () => {
+  return (
+    <div style={{ padding: "0px 15px 0px 15px" }}>
+      <h1 style={{ margin: "0px 0px 10px 0px", textAlign: "center" }}>
+        Edit Task
+      </h1>
+      <div style={{ margin: "0px 0px 10px 0px", textAlign: "center" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+        molestie magna risus, sit amet hendrerit augue maximus et. Nam
+        fermentum, mi nec posuere malesuada, massa orci gravida enim, at euismod
+        arcu ante ac enim. Donec rutrum tempus molestie. Quisque bibendum
+        bibendum aliquet. Morbi nec mauris scelerisque, ultrices massa vel,
+        accumsan dolor. Etiam pulvinar dignissim ipsum, nec consequat massa
+        finibus vitae. Etiam sit amet augue sodales, auctor metus accumsan,
+        fringilla ex. Integer et sodalesorci. Integer eleifend eu tellus et
+        consectetur. Aliquam mollis vehicula nulla, eget consectetur nunc.
+      </div>
+      <div>
+        <button>Click here</button>
+      </div>
+    </div>
+  );
+};
+export default EditTask;
