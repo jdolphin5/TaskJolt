4f12bb3527ac9ea50feae3aed9bcb93bfc4e5a12
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-03 12:04:41 +1100

    added antd table with dummy data

---
+++ client/src/Tasks.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { Table, Button } from "antd";
+import { Link } from "react-router-dom";
+
+const Tasks: React.FC = () => {
+  const columns = [
+    {
+      title: "Project Name",
+      dataIndex: "projectName",
+      key: "projectName",
+    },
+    {
+      title: "Task Name",
+      dataIndex: "taskName",
+      key: "taskName",
+      //the render function receives three arguments from the antd table component
+      render: (text: string, data: any, index: number) => (
+        <Link to={`/EditTask/${data.key}`}>{data.taskName}</Link>
+      ),
+    },
+    {
+      title: "Priority",
+      dataIndex: "priority",
+      key: "priority",
+    },
+    {
+      title: "Due Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Due Time",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "Recurring",
+      dataIndex: "recurring",
+      key: "recurring",
+    },
+  ];
+
+  const taskData = [
+    {
+      key: "1",
+      projectName: "Project 1",
+      taskName: "Task 1",
+      priority: "High",
+      date: "12/12/12",
+      time: "09:00",
+      recurring: "yes",
+    },
+    {
+      key: "2",
+      projectName: "Project 1",
+      taskName: "Task 2",
+      priority: "Medium",
+      date: "1/6/12",
+      time: "09:00",
+      recurring: "yes",
+    },
+    {
+      key: "3",
+      projectName: "Project 1",
+      taskName: "Task 3",
+      priority: "Low",
+      date: "1/3/12",
+      time: "09:00",
+      recurring: "no",
+    },
+  ];
+
+  return (
+    <div style={{ padding: "0px 15px 0px 15px" }}>
+      <h1 style={{ margin: "0px 0px 10px 0px", textAlign: "center" }}>
+        Task List
+      </h1>
+      <div style={{ margin: "0px 0px 10px 0px" }}>
+        <Table dataSource={taskData} columns={columns} />
+        <Button>Add task</Button>
+        <p>To edit a task, simply click on the task name.</p>
+      </div>
+    </div>
+  );
+};
+export default Tasks;
