95d34ab15f220fcef89718a05dc62819119f4f9c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-03 12:03:26 +1100

    added Tasks and EditTask route

--- client/src/ContentArea.tsx
+++ client/src/ContentArea.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Tasks from "./Tasks";
+import EditTask from "./EditTask";
 
 const ContentArea: React.FC = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="tasks" element={<Tasks />} />
+      <Route path="/edittask/:key" element={<EditTask />} />
+    </Routes>
   );
 };
 export default ContentArea;
