abfb584a6d9c627290c412095989bb9bd7d513bd
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-07 09:07:57 +1000

    prisma test - fetch users from db

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,28 +6,30 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = mysql.createPool({
-  connectionLimit: process.env.CONNECTION_LIMIT,
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const { PrismaClient } = require("@prisma/client");
 
-pool.query(
-  "SELECT * FROM task",
-  function (error: any, results: any, fields: any) {
-    if (error) throw error;
-    console.log(results);
-  }
-);
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.users.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e: any) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 // Serve static files from the 'dist' folder
 const distPath = path.resolve(__dirname, "../client/dist");
 app.use(express.static(distPath));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Start the server
 app.listen(port, () => {
