0ed15253942fe83b7d0a9a476d6d274148427d4b
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-06 16:17:07 +1100

    mysql pool connected, first query

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,26 @@ const path = require("path"); // Add the 'path' module
 const express = require("express");
 const app = express();
 
-console.log("test");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pool = mysql.createPool({
+  connectionLimit: process.env.CONNECTION_LIMIT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+pool.query(
+  "SELECT * FROM task",
+  function (error: any, results: any, fields: any) {
+    if (error) throw error;
+    console.log(results);
+  }
+);
 
 // Serve static files from the 'dist' folder
 const distPath = path.resolve(__dirname, "../client/dist");
