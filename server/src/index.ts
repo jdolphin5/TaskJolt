2a95606ee1f67fbe6c5c9039f20d79bfcb4b6b08
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-09 12:03:03 +1000

    api.get to retrieve notes - untested

--- server/src/index.ts
+++ server/src/index.ts
@@ -88,6 +88,39 @@ app.get("/api/tasks", async (req: any, res: any) => {
   }
 });
 
+app.get("/api/notes/:id", async (req: any, res: any) => {
+  try {
+    async function main() {
+      const { tasksObject } = req.query;
+
+      const taskIdArray = tasksObject.split(",").map(Number);
+
+      const data = await prisma.notes.findMany({
+        where: {
+          task_id: {
+            in: taskIdArray,
+          },
+        },
+      });
+      res.json(data);
+    }
+
+    main()
+      .then(async () => {
+        await prisma.$disconnect();
+      })
+      .catch(async (e: any) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+      });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // Serve static files from the 'dist' folder
 const distPath = path.resolve(__dirname, "../client/dist");
 app.use(express.static(distPath));
