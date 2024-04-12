a6692e46f1e8297b92ef23959f2fd625a147f683
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-12 13:35:58 +1000

    add project - component re-renders on form submit

--- client/src/components/AddProjectModal.tsx
+++ client/src/components/AddProjectModal.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Table, Button, Form, Input } from "antd";
 import axios from "axios";
+import { TasksProps } from "./Types";
 
-interface AddProjectProps {
+interface AddProjectProps extends TasksProps {
   showHideAddProjectModal: boolean;
   setShowHideAddProjectModal: React.Dispatch<React.SetStateAction<boolean>>;
   projectsLoaded: boolean;
@@ -11,13 +12,30 @@ interface AddProjectProps {
   setProjectData: React.Dispatch<React.SetStateAction<any>>;
 }
 
+async function fetchProjectData() {
+  try {
+    const response = await axios.get("http://localhost:3000/api/projects");
+
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
 const AddProjectModal: React.FC<AddProjectProps> = ({
-  showHideAddProjectModal,
-  setShowHideAddProjectModal,
+  tasksPageLoaded,
+  setTasksPageLoaded,
   projectsLoaded,
   setProjectsLoaded,
+  tasksLoaded,
+  setTasksLoaded,
   projectData,
   setProjectData,
+  taskData,
+  setTaskData,
+  showHideAddProjectModal,
+  setShowHideAddProjectModal,
 }) => {
   const handleOk = () => {
     setShowHideAddProjectModal(false);
@@ -60,9 +78,9 @@ const AddProjectModal: React.FC<AddProjectProps> = ({
   useEffect(() => {
     if (projectsLoaded) {
       formatProjectData(projectData);
-      console.log(projectData);
     }
-  }, []);
+    console.log("Updated projectData:", projectData);
+  }, [, projectData]);
 
   const columns = [
     {
@@ -79,7 +97,7 @@ const AddProjectModal: React.FC<AddProjectProps> = ({
 
   const [formData, setFormData] = useState<{ name?: string }>({});
 
-  const handleInputChange = (e: any) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -87,14 +105,28 @@ const AddProjectModal: React.FC<AddProjectProps> = ({
   const handleSubmit = async () => {
     try {
       console.log(formData);
-      // Make API call using axios
+
       const response = await axios.post(
-        "http://localhost:3000/api/addproject/",
+        "http://localhost:3000/api/addproject",
         formData
       );
       console.log("API Response:", response.data);
-      // Reset form after successful submission
-      setFormData({});
+
+      setFormData({ name: "" });
+
+      fetchProjectData()
+        .then((projectData) => {
+          console.log("success");
+
+          const filteredProjectIds = projectData?.filter(
+            (project: { id: number }) => project.id !== 2
+          );
+
+          setProjectData(filteredProjectIds);
+        })
+        .catch((error) => {
+          console.error("Error fetching project data:", error);
+        });
     } catch (error) {
       console.error("Error submitting form:", error);
     }
