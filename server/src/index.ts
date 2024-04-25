6128a0a8b49fce7270efb9be8d8eaac9b7f44970
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-26 09:45:55 +1000

    app.put for edittask implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -187,6 +187,50 @@ app.post("/api/addtask", async (req: any, res: any) => {
   }
 });
 
+app.put("/api/edittask/:id", async (req: any, res: any) => {
+  const taskId = parseInt(req.params.id);
+  const formData = req.body;
+
+  console.log("Received form data:", formData);
+
+  if (formData === undefined) {
+    console.log("received req.body is undefined");
+  }
+  //name, priority, start_date_time, due_date_time, recurring, project_id
+  try {
+    async function main() {
+      const isoStartDate = new Date(formData.start_date_time).toISOString();
+      const isoDueDate = new Date(formData.due_date_time).toISOString();
+
+      const updatedTask = await prisma.task.update({
+        where: { id: taskId },
+        data: {
+          name: formData.name,
+          priority: formData.priority,
+          start_date_time: isoStartDate,
+          due_date_time: isoDueDate,
+          recurring: formData.recurring,
+          project_id: formData.project,
+        },
+      });
+      console.log("task updated:", updatedTask);
+      res.status(200).json({ message: "Form data received successfully!" });
+    }
+
+    main()
+      .then(async () => {
+        await prisma.$disconnect();
+      })
+      .catch(async (e: any) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+      });
+  } catch (error) {
+    console.error("Error creating project:", error);
+  }
+});
+
 // Serve static files from the 'dist' folder
 const distPath = path.resolve(__dirname, "../client/dist");
 app.use(express.static(distPath));
