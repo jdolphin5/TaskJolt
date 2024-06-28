3a40c6d7369ff484d43b68150a8fe514c8103767
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-06-28 15:21:53 +1000

    add network diagram prop type to include projectId

--- client/src/Types.ts
+++ client/src/Types.ts
@@ -13,6 +13,21 @@ export interface TasksProps {
   setTaskData: React.Dispatch<React.SetStateAction<any>>;
 }
 
+export interface NetworkDiagramProps {
+  tasksPageLoaded: boolean;
+  setTasksPageLoaded: React.Dispatch<React.SetStateAction<boolean>>;
+  projectsLoaded: boolean;
+  setProjectsLoaded: React.Dispatch<React.SetStateAction<boolean>>;
+  tasksLoaded: boolean;
+  setTasksLoaded: React.Dispatch<React.SetStateAction<boolean>>;
+  projectData: any;
+  setProjectData: React.Dispatch<React.SetStateAction<any>>;
+  taskData: any;
+  setTaskData: React.Dispatch<React.SetStateAction<any>>;
+  projectId: number;
+  setProjectId: React.Dispatch<React.SetStateAction<number>>;
+}
+
 export interface TasksTableProps {
   dataSource: any;
   columns: any;
