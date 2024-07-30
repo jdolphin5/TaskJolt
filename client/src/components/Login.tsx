b297c78780a047d79694719b0cbf6395ca1dd74c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-07-30 13:16:17 +1000

    axios unused

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Button, Input, Form } from "antd";
-import axios from "axios";
-
-/*import {
-  hasGrantedAllScopesGoogle,
-  GoogleOAuthProvider,
-  GoogleLogin,
-} from "@react-oauth/google";
-*/
 
 import googleButton from "../assets/google_signin_buttons/web/1x/btn_google_signin_dark_pressed_web.png";
 
