bab628553e713211f387c35c1135d07f4609503d
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-07-30 13:09:31 +1000

    passport userData received

--- client/src/components/LoggedIn.tsx
+++ client/src/components/LoggedIn.tsx
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const LoggedIn: React.FC = () => {
-  const query = useQuery();
   const [userData, setUserData] = useState<any>(null);
 
-  const dataJSON = query.get("userData");
+  const getUserData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/profile`);
+      //console.log("API Response:", response.data);
+      setUserData(response.data);
+      return response.data;
+    } catch (error: any) {
+      console.error("Cannot get user data from /api/profile/", error);
+    }
+  };
 
   useEffect(() => {
-    if (dataJSON) {
-      const data = JSON.parse(decodeURIComponent(dataJSON));
-      setUserData(data);
-    }
-  }, [dataJSON]);
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
   return (
     <div style={{ padding: "0px 15px 0px 15px" }}>
       <h1 style={{ textAlign: "center" }}>Logged In</h1>
       <p>Some text</p>
-      {userData ? <div>Welcome {userData.name}</div> : <div></div>}
+      {userData ? <div>Welcome {userData.email}</div> : <div></div>}
     </div>
   );
 };
